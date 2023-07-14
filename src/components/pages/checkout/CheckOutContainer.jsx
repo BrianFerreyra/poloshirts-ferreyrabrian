e04cd1eb94d0fe46e1bd1eebeafa6c0861623a3e
BrianFerreyra <brian_ferreyra@live.com>
2023-07-14 05:23:07 -0300

    proyecto final more fix

--- src/components/pages/checkout/CheckOutContainer.jsx
+++ src/components/pages/checkout/CheckOutContainer.jsx
@@ -8,7 +8,7 @@ import { CartContext } from "../../../context/CartContext";
 import "./CheckOut.Module.css";
 
 const CheckOutContainer = () => {
-  const { cart, totalPrice, clearCart } = useContext(CartContext);
+  const { cart, totalPrice } = useContext(CartContext);
   const [orderId, setOrderId] = useState(null);
   let total = totalPrice();
   const { handleSubmit, handleChange, errors } = useFormik({
@@ -32,8 +32,6 @@ const CheckOutContainer = () => {
           stock: product.stock - product.quantity,
         });
       });
-
-      clearCart();
     },
     validateOnChange: false,
     validationSchema: Yup.object({
